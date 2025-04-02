0b8ca7330ec20842d03d69e31c99a531594c4229
Leone, Mark A [LGS] <mleone607@gmail.com>
2025-04-02 10:35:15 -0400

    Binary search working for part 2

--- solutions/2024/day_18/solution.py
+++ solutions/2024/day_18/solution.py
@@ -7,7 +7,6 @@ from ...utils.transformations import parse_ints
 from ...utils.graphs import neighbors, GridPoint, taxicab_distance
 import re
 import heapq
-from collections import defaultdict
 
 class Solution(StrSplitSolution):
     _year = 2024
@@ -28,16 +27,16 @@ class Solution(StrSplitSolution):
                     self.dfs(nb, end, cost+1, visited.copy())
     
     def bfs(self, start: GridPoint, end: GridPoint):
-        min_cost = None
+        min_dst = None
         open_list = []
         path = []
 
-        heapq.heappush(open_list, (0, 0, start, [], set()))
+        heapq.heappush(open_list, (0, start, [], set()))
         while open_list:
-            dst, cost, pos, _path, visited = heapq.heappop(open_list)
+            dst, pos, _path, visited = heapq.heappop(open_list)
             if pos == end:
-                if min_cost is None or cost < min_cost:
-                  min_cost = cost
+                if min_dst is None or dst < min_dst:
+                  min_dst = dst
                   path = _path
                 continue
             if pos in visited:
@@ -46,30 +45,26 @@ class Solution(StrSplitSolution):
             for nb in neighbors(pos, num_directions=4, max_size=self.max_size):
                 if nb not in self.bytes:
                     if nb not in visited:
-                        heapq.heappush(open_list, (dst + taxicab_distance(nb, end), cost+1, nb, _path + [nb], visited))
-        return min_cost, path
+                        heapq.heappush(open_list, (dst+1, nb, _path + [nb], visited))
+        return min_dst, path
+    
+    def bfsa(self, start: GridPoint, end: GridPoint):
+        seen = set([start])
+        path = [start]
+        queue = [(start, path)]
+        while len(queue):
+            next_q = []
+            for pos, path in queue:
+                if pos == end:
+                    return path
+                for nb in neighbors(pos, num_directions=4, max_size=self.max_size):
+                    if nb not in self.bytes and nb not in seen:
+                        seen.add(nb)
+                        next_q.append((nb,  path + [nb]))
+            queue = next_q
+        return []
 
-    def bfs2(self, start: GridPoint, end: GridPoint):
-      open_list = []
-      paths = []
-
-      heapq.heappush(open_list, (0, [], start, {}))
-      while open_list:
-          dst, path, pos, visited = heapq.heappop(open_list)
-          if pos == end:
-              paths.append(path)
-              visited = set()
-              continue
-          if pos in visited:
-            continue
-          visited[pos] = dst
-          for nb in neighbors(pos, num_directions=4, max_size=self.max_size):
-              if nb not in self.bytes:
-                  if nb not in visited or visited[nb] >= dst:
-                    heapq.heappush(open_list, (dst + taxicab_distance(nb, end), path[::]+[nb], nb, visited))
-      return paths
-
-    # @answer(298)
+    @answer(298)
     def part_1(self) -> int:
         self.max_size = 6 if self.use_test_data else 70
         self.n = 12 if self.use_test_data else 1024
@@ -87,28 +82,26 @@ class Solution(StrSplitSolution):
             if left == right:
                 return self._bytes[left][::-1]
             middle = (left + right) // 2
-            self.bytes = self._bytes[:self.n] + self._bytes[left: middle+1]
-            paths = self.bfs2(self.start, self.end)
-            if len(paths) == 0:
-                print(self.bytes[self.n:])
+            self.bytes = self._bytes[0: middle+1]
+            path = self.bfsa(self.start, self.end)
+            if len(path) == 0:
                 right = middle - 1
             else:
                 left = middle + 1
         return None
       
-    # @answer(52,32)
+    @answer((52,32))
     def part_2(self) -> int:
-        # This does not work, but seems like it should
-        # return self.find_breaking_idx(0, len(self._bytes) -1)
+        return self.find_breaking_idx(0, len(self._bytes) -1)
 
-        #This works, but it's a semi brute-force solution that takes 20 secs
-        path = None
-        for b in self._bytes[self.n:]:
-            self.bytes.append(b)
-            if path is None or b in path:
-                _, path = self.bfs(self.start, self.end)
-                if not len(path):
-                    return b[::-1]
+        #This works, but it's a semi brute-force solution that takes 16 secs
+        # path = None
+        # for b in self._bytes[self.n:]:
+        #     self.bytes.append(b)
+        #     if path is None or b in path:
+        #         path = self.bfsa(self.start, self.end)
+        #         if not len(path):
+        #             return b[::-1]
 
     # @answer((1234, 4567))
     # def solve(self) -> tuple[int, int]:
