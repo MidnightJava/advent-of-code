292f9886912e8ad46c049980906dc2d2d4e14c3a
Leone, Mark A [LGS] <mleone607@gmail.com>
2025-12-28 22:50:52 -0500

    Day 10 part 1

--- solutions/2025/day_10/solution.py
+++ solutions/2025/day_10/solution.py
@@ -5,6 +5,7 @@
 from ...base import StrSplitSolution, answer
 from ...utils.transformations import parse_ints
 import re
+from itertools import product
 
 class Machine:
 
@@ -19,7 +20,22 @@ class Solution(StrSplitSolution):
 
     machines = []
 
-    # @answer(1234)
+    def min_presses(self, configs, machine):
+        min_count = None
+        for config in configs:
+            l = list('.' * len(machine.lights))
+            for i, c in enumerate(config):
+                #press buttons[i] c times
+                btn = machine.buttons[i]
+                if c % 2 != 0:
+                    for b in btn:
+                        l[b] = '.' if l[b] == '#' else '#'
+            if l == machine.lights:
+                num_presses = sum(config)
+                min_count = num_presses if not min_count else min(num_presses, min_count)
+        return min_count
+
+    @answer(514)
     def part_1(self) -> int:
         for line in self.input:
             lights = re.findall(r'[\.#]', line)
@@ -27,13 +43,12 @@ class Solution(StrSplitSolution):
             buttons = [tuple(map(int, x.split(','))) for x in buttons]   
             machine = Machine(lights, buttons)
             self.machines.append(machine)
+        count = 0
         for machine in self.machines:
-            for light in machine.lights:
-                print(light, end='')
-            print("\n")
-            for button in machine.buttons:
-                print(button, type(button))
-            print("-----------------")
+            n = len(machine.buttons)
+            button_configs = product(range(3), repeat=n)
+            count+= self.min_presses(list(button_configs), machine)
+        return count
 
     # @answer(1234)
     def part_2(self) -> int:
